3adf71db330cdc60607c6556e6aa184cb8d8f9d3
Alok Kumar <kec.alok@gmail.com>
2020-04-14 14:44:15 +0530

    test

--- src/main/java/com/alok/app/Test.java
+++ src/main/java/com/alok/app/Test.java
@@ -4,12 +4,12 @@ public class Test {
 	public void print1() {
 		//------n
 		//------n
-
+		//line 7
 	}
 	//test comments 
 	//
+	//line 11
 	public void print2() {
-		//---d
 		//----d
 		//----d
 	}

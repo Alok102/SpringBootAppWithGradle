4f8898ef916b97ea0dd80e3b4f9a2ca207aa6a3e
Alok Kumar <kec.alok@gmail.com>
2020-04-14 14:36:31 +0530

    test

---
+++ src/main/java/com/alok/app/Test.java
@@ -0,0 +1,18 @@
+package com.alok.app;
+
+public class Test {
+	public void print1() {
+		//------n
+		//------n
+		//-----------
+		//------
+	}
+	public void print2() {
+		//---d
+		//-----------
+	}
+	public void print3() {
+		//------
+		//-----------
+	}
+}

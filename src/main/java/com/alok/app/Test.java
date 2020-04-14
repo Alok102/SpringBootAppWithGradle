2ee1dbd2574fef041c8515b8835c3deb916b0772
Alok Kumar <kec.alok@gmail.com>
2020-04-14 14:38:30 +0530

    test

--- src/main/java/com/alok/app/Test.java
+++ src/main/java/com/alok/app/Test.java
@@ -4,12 +4,12 @@ public class Test {
 	public void print1() {
 		//------n
 		//------n
-		//-----------
-		//------
+
 	}
 	public void print2() {
 		//---d
-		//-----------
+		//----d
+		//----d
 	}
 	public void print3() {
 		//------

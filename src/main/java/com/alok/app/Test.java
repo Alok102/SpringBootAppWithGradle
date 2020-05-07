7fd2caeeec0edfc06a9fb5673d73b34574fc09b6
Alok Kumar <kec.alok@gmail.com>
2020-05-07 14:38:04 +0530

    test

--- src/main/java/com/alok/app/Test.java
+++ src/main/java/com/alok/app/Test.java
@@ -5,22 +5,38 @@ import java.util.stream.IntStream;
 public class Test {
 	
 	public int sum(int a, int b) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
 		return (a+b);
 	}
 
 	public void sub(int a , int b) {
 		System.out.println("Subs" + (a - b));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
 	}
 
 	public void printRandomNumber() {
 		IntStream stream = IntStream.generate(() -> {
+			System.out.println();
 			return (int) (Math.random() * 10000);
 		});
 
 		stream.limit(10).forEach(System.out::println);
+		System.out.println();
 	}
 
 	public void div(int a , int b) {
+		System.out.println();
 		System.out.println("Div" + (a / b));
 	}
 }
